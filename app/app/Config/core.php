50bb560788ed5899ff4972b8f801349cc458ca9c
bakalarka <marek.drnzik@gmail.com>
2011-12-01 12:10:52 +0100

    Prvotne nastavenie config files

--- app/app/Config/core.php
+++ app/app/Config/core.php
@@ -184,12 +184,13 @@
 /**
  * A random string used in security hashing methods.
  */
-	Configure::write('Security.salt', 'DYhG93b0qyJfIxfs2guVoUubWwvniR2G0FgaC9mi');
+	Configure::write('Security.salt', 'FScs6SdS9SD5s0sQ2cspBdUk49bhcCSHcbD5ha3D');
 
 /**
  * A random numeric string (digits only) used to encrypt/decrypt strings.
  */
 	Configure::write('Security.cipherSeed', '76859309657453542496749683645');
+	Configure::write('Security.cipherSeed', '65764354687312313978843720217');
 
 /**
  * Apply timestamps with the last modified time to static assets (js, css, images).
