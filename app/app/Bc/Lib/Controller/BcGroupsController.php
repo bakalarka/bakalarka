50f6df56819344decfac5c2dc93c3c0e32982967
bakalarka <marek.drnzik@gmail.com>
2012-04-22 22:03:38 +0200

    Added blank methods to BcGroupsController.php

--- app/app/Bc/Lib/Controller/BcGroupsController.php
+++ app/app/Bc/Lib/Controller/BcGroupsController.php
@@ -5,6 +5,45 @@ class BcGroupsController extends AppController {
 	var $uses 		= array(
 		'Group'
 	);
+	
+	function beforeFilter() {
+	    parent::beforeFilter();
+	    $this->Auth->allow('*');
+	}
+	
+	/** backend **/
+	
+	/**
+	 * admin_index
+	 * Groups listing
+	 */
+	function admin_index() {
+		//
+	}
+	
+	/**
+	 * admin_add
+	 * Add new group
+	 */
+	function admin_add() {
+		//
+	}
+	
+	/**
+	 * admin_edit
+	 * Edit group
+	 */
+	function admin_edit() {
+		//
+	}
+	
+	/**
+	 * admin_remove
+	 * Remove group
+	 */
+	function admin_remove() {
+		//
+	}
 		
 }
 ?>
