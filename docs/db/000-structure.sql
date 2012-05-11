55836005a5260b8fe21ae2bf4df928bd54711ed5
marek <marek.drnzik@gmail.com>
2012-05-11 19:40:17 +0200

    Add i18n table. Used with Translate behavior.

--- docs/db/000-structure.sql
+++ docs/db/000-structure.sql
@@ -228,6 +228,24 @@ CREATE TABLE `groups` (
   PRIMARY KEY (`id`)
 ) ENGINE=InnoDB AUTO_INCREMENT=6 DEFAULT CHARSET=utf8;
 
+/*Table structure for table `i18n` */
+
+DROP TABLE IF EXISTS `i18n`;
+
+CREATE TABLE `i18n` (
+  `id` int(10) NOT NULL AUTO_INCREMENT,
+  `locale` varchar(6) NOT NULL,
+  `model` varchar(255) NOT NULL,
+  `foreign_key` int(10) NOT NULL,
+  `field` varchar(255) NOT NULL,
+  `content` mediumtext,
+  PRIMARY KEY (`id`),
+  KEY `locale` (`locale`),
+  KEY `model` (`model`),
+  KEY `row_id` (`foreign_key`),
+  KEY `field` (`field`)
+) ENGINE=InnoDB DEFAULT CHARSET=utf8;
+
 /*Table structure for table `image_galleries` */
 
 DROP TABLE IF EXISTS `image_galleries`;
