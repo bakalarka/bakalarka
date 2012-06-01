36042216ba4a9716a6f2b7b1f40788b1d3624f46
marek <marek.drnzik@gmail.com>
2012-06-01 17:06:40 +0200

    Remove Text.php and TextRecord.php

--- app/app/Model/Text.php
+++
@@ -1,6 +0,0 @@
-<?php
-App::uses('BcText', 'Bc/Model');
-class Text extends BcText {
-	var $name = 'Text';
-}
-?>
--- app/app/Model/TextRecord.php
+++
@@ -1,6 +0,0 @@
-<?php
-App::uses('BcTextRecord', 'Bc/Model');
-class TextRecord extends BcTextRecord {
-	var $name = 'TextRecord';
-}
-?>
