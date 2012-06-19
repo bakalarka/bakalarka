57b8a667209351a67fd5f84e372c62c08b30b78a
marek <marek.drnzik@gmail.com>
2012-06-19 18:01:16 +0200

    Add test DB structure.sql

---
+++ docs/db_test/structure.sql
@@ -0,0 +1,15 @@
+DROP TABLE IF EXISTS `i18n`;
+
+CREATE TABLE `i18n` (
+  `id` int(10) NOT NULL AUTO_INCREMENT,
+  `locale` varchar(6) NOT NULL,
+  `model` varchar(255) NOT NULL,
+  `foreign_key` int(10) NOT NULL,
+  `field` varchar(255) NOT NULL,
+  `content` mediumtext,
+  PRIMARY KEY (`id`),
+  KEY `locale` (`locale`),
+  KEY `model` (`model`),
+  KEY `row_id` (`foreign_key`),
+  KEY `field` (`field`)
+) ENGINE=InnoDB DEFAULT CHARSET=utf8;
